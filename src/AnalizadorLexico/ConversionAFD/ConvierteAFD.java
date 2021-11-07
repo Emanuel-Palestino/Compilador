ddcfc24252f6da6e654be39f4a440c23498212e6
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2021-11-07 16:13:00 -0600

    Conversion a AFD modificado

--- src/AnalizadorLexico/ConversionAFD/ConvierteAFD.java
+++ src/AnalizadorLexico/ConversionAFD/ConvierteAFD.java
@@ -8,90 +8,98 @@ import Utilidades.Listas.ListaDoblementeEnlazadaD;
 
 public class ConvierteAFD {
 
+	public static void main(String[] args) {
 
-  public static void main(String[] args) {
+		Thompson thomp = new Thompson();
+		Automata a = new Automata("a");
+		Automata b = new Automata("b");
+		Automata aob = thomp.union(a, b);
+		Automata aobk = thomp.cerraduraKlenee(aob);
+		Automata aobka = thomp.concatenacion(aobk, a);
+		Automata aobkab = thomp.concatenacion(aobka, b);
+		Automata res = thomp.concatenacion(aobkab, b);
 
-    Thompson thomp = new Thompson();
-    Automata a = new Automata("a");
-    Automata b = new Automata("b");
-    Automata aob = thomp.union(a, b);
-    Automata aobk = thomp.cerraduraKlenee(aob);
-    Automata aobka = thomp.concatenacion(aobk, a);
-    Automata aobkab = thomp.concatenacion(aobka, b);
-    Automata res = thomp.concatenacion(aobkab, b);
+		ConvierteAFD afd = new ConvierteAFD();
+		ArrayList<ListaDoblementeEnlazadaD> resultado = afd.convierteAFD(res);
+		System.out.println("a");
 
-    ConvierteAFD afd = new ConvierteAFD();
-    ArrayList<ListaDoblementeEnlazadaD> resultado = afd.convierteAFD(res);
-    System.out.println("a");
+	}
 
-  }
+	// Identificador de conjunto de estados "A", "B"
+	public ArrayList<ListaDoblementeEnlazadaD> convierteAFD(Automata automata) {
 
-  //Identificador de conjunto de estados "A", "B"
-  public ArrayList<ListaDoblementeEnlazadaD> convierteAFD(Automata automata) {
+		ArrayList<ListaDoblementeEnlazadaD> tranD = new ArrayList<ListaDoblementeEnlazadaD>();
+		ArrayList<ConjuntoEstados> estadosD = new ArrayList<ConjuntoEstados>();
+		char letra = 'A';
 
-    ArrayList<ListaDoblementeEnlazadaD> tranD = new ArrayList<ListaDoblementeEnlazadaD>();
-    ArrayList<ConjuntoEstados> estadosD = new ArrayList<ConjuntoEstados>();
-    char letra = 'A';
-    
-    //Suponiendo que Rugal hara un metodo cerradura para la clase cerraduraEpsilon
-    ConjuntoEstados inicio = new ConjuntoEstados();
-    inicio.insertarEstado(0);
-    estadosD.add(CerraduraEpsilon.doCerraduraEpsilon(inicio, automata));
-    estadosD.get(0).setId("A");
-    
-    //recorrer la lista; mientras no haya marcados o llegemos al final de la lista
-    for (int i = 0; i < estadosD.size() && estadosD.get(i).getMarcado() == false; i++){
-      
-      estadosD.get(i).setMarcado(true); //Se marca el estado T
-      
-      ArrayList <String> transiciones = new ArrayList<String>();
+		// Suponiendo que Rugal hara un metodo cerradura para la clase cerraduraEpsilon
+		ConjuntoEstados inicio = new ConjuntoEstados();
+		inicio.insertarEstado(0);
+		estadosD.add(CerraduraEpsilon.doCerraduraEpsilon(inicio, automata));
+		estadosD.get(0).setId("A");
 
-      for (Integer estado : estadosD.get(i).getEstados()) {
-        
-        for(String transicion: automata.getSimbolosTransiciones(estado)){
+		// Comprobar si el primer estado es un estado final
+		if (estadosD.get(0).getEstados().contains(automata.getNumEstados() - 1)) {
+			estadosD.get(0).setEstadoFinal(true);
+		}
+		// recorrer la lista; mientras no haya marcados o llegemos al final de la lista
+		for (int i = 0; i < estadosD.size() && estadosD.get(i).getMarcado() == false; i++) {
 
-          if(!transicion.equals("Ɛ") && !transiciones.contains(transicion))
-            transiciones.add(transicion);
-        }
-        //Usar for each para recorrer array de stirng que devuelve getSimbolosTransiciones
-        //Y meter a transiciones
-      }
+			estadosD.get(i).setMarcado(true); // Se marca el estado T
 
-      for(int k = 0; k < transiciones.size(); k++){
-        
-        ConjuntoEstados U = new ConjuntoEstados();
-        // Suponiendo que Roborto hace un metodo mover para la clase mueve
-        U = CerraduraEpsilon.doCerraduraEpsilon(Mueve.mueve(estadosD.get(i), transiciones.get(k), automata),automata);
-        Boolean bandera = true;
-        for (ConjuntoEstados a : estadosD) {
-          if(U.getEstados().equals(a.getEstados())){
-            U = a;
-            bandera = false;
-          }
-        }
-        if(bandera){ //Index of regresa -1 si U no se encuentra en el Arreglo
-          
-          letra++;
-          U.setId(letra +"");
-          U.setMarcado(false);
-          estadosD.add(U);
-        }
+			ArrayList<String> transiciones = new ArrayList<String>();
 
-        if(i == tranD.size()){
-          ListaDoblementeEnlazadaD lista = new ListaDoblementeEnlazadaD();
-          lista.insertar(U, transiciones.get(k));
-          lista.setEstado(estadosD.get(i));
-          tranD.add(lista);
-        }else{
-          tranD.get(i).insertar(U, transiciones.get(k));
-        }
-        
-      }
-    }
-    
-    return tranD;
-  
-  }
+			for (Integer estado : estadosD.get(i).getEstados()) {
 
+				for (String transicion : automata.getSimbolosTransiciones(estado)) {
+
+					if (!transicion.equals("Ɛ") && !transiciones.contains(transicion))
+						transiciones.add(transicion);
+				}
+				// Usar for each para recorrer array de stirng que devuelve
+				// getSimbolosTransiciones
+				// Y meter a transiciones
+			}
+
+			for (int k = 0; k < transiciones.size(); k++) {
+
+				ConjuntoEstados U = new ConjuntoEstados();
+				// Suponiendo que Roborto hace un metodo mover para la clase mueve
+				U = CerraduraEpsilon.doCerraduraEpsilon(Mueve.mueve(estadosD.get(i), transiciones.get(k), automata),
+						automata);
+				Boolean bandera = true;
+				for (ConjuntoEstados a : estadosD) {
+					if (U.getEstados().equals(a.getEstados())) {
+						U = a;
+						bandera = false;
+					}
+				}
+				if (bandera) { // Index of regresa -1 si U no se encuentra en el Arreglo
+
+					letra++;
+					U.setId(letra + "");
+					U.setMarcado(false);
+					// Lo marca si es un estado final
+					if (U.getEstados().contains(automata.getNumEstados() - 1))
+						U.setEstadoFinal(true);
+
+					estadosD.add(U);
+				}
+
+				if (i == tranD.size()) {
+					ListaDoblementeEnlazadaD lista = new ListaDoblementeEnlazadaD();
+					lista.insertar(U, transiciones.get(k));
+					lista.setEstado(estadosD.get(i));
+					tranD.add(lista);
+				} else {
+					tranD.get(i).insertar(U, transiciones.get(k));
+				}
+
+			}
+		}
+
+		return tranD;
+
+	}
 
 }
--- src/AnalizadorLexico/ConversionAFD/Mueve.java
+++ src/AnalizadorLexico/ConversionAFD/Mueve.java
@@ -2,18 +2,19 @@ package AnalizadorLexico.ConversionAFD;
 
 import java.util.ArrayList;
 
-import AnalizadorLexico.AlgoritmoThompson.Thompson;
 import Utilidades.Automata;
 import Utilidades.ConjuntoEstados;
+import Utilidades.Listas.ListaDoblementeEnlazadaD;
+import Utilidades.Listas.NodoListaD;
 
 public class Mueve {
-    public static ConjuntoEstados mueve(ConjuntoEstados T, String simbolo, Automata afn) {
+    public static ConjuntoEstados mueve(ConjuntoEstados T, String simbolo, Automata AFN) {
         ConjuntoEstados resultado = new ConjuntoEstados();
         ArrayList<Integer> auxiliar = T.getEstados();
         int[] arrayAux;
 
         for (int i = 0; i < auxiliar.size(); i++) {
-            arrayAux = afn.getEstadosDestinoSimbolo(auxiliar.get(i), simbolo);
+            arrayAux = AFN.getEstadosDestinoSimbolo(auxiliar.get(i), simbolo);
             for (int estado : arrayAux) {
                 resultado.getEstados().add(estado);
             }
@@ -21,15 +22,26 @@ public class Mueve {
         return resultado;
     }
 
-    public static void main(String[] args) {
-        Automata afd = new Automata("a");
-        Automata afd2 = new Automata("b");
-        Thompson aux = new Thompson();
-        Automata ejemplo = aux.concatenacion(afd, afd2);
-        ConjuntoEstados estados = new ConjuntoEstados();
-        estados.getEstados().add(0);
-        
-        ConjuntoEstados resultado = Mueve.mueve(estados, "a" , ejemplo);
-        System.out.println("Resultado ");
+    public static ConjuntoEstados mueve(ConjuntoEstados T, String simbolo, ArrayList<ListaDoblementeEnlazadaD> AFD) {
+        ConjuntoEstados resultado = new ConjuntoEstados();
+        ListaDoblementeEnlazadaD estado = new ListaDoblementeEnlazadaD();
+        for(ListaDoblementeEnlazadaD lista : AFD) {
+           if (lista.getEstado().equals(T)) {
+               estado = lista;
+               break;
+           } 
+        }
+
+        NodoListaD aux = estado.getInicio();
+        while(aux != null) {
+            if (aux.getTransicion().equals(simbolo)) {
+                resultado = aux.getEstados();
+                break;
+            }
+            aux = aux.getSiguiente();
+        }
+
+        return resultado;
     }
+
 }
