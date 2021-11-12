4feb3a0cc307423162f0b212bf003eb8108d2d04
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2021-11-12 13:22:12 -0600

    [Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,14 +2,11 @@ package Main;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
-
-import AnalizadorLexico.ConversionAFD.IniciarConjuntos;
-import AnalizadorLexico.Final.IniciarFinal;
 import Utilidades.Excepciones.ExcepcionER;
+
 public class Main {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ExcepcionER {
@@ -20,8 +17,7 @@ public class Main {
 		} catch (Exception e) {
 			System.out.println("Error al aplicar estilo Windows");
 		}
-		//new IniciarThompson();
-		//new IniciarConjuntos();
-		new IniciarFinal();
+
+		new VentanaPrincipal().setVisible(true);
 	}
 }
