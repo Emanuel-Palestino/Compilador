3bda94cffb2846841d02128c42e85f8b4e6d6665
snifex <noblseis88@gmail.com>
2021-11-17 23:42:31 -0600

    Algoritmo siguiente bosquejo

--- src/AnalizadorSintactico/PrimerosSiguientes/PrimerosSiguientes.java
+++ src/AnalizadorSintactico/PrimerosSiguientes/PrimerosSiguientes.java
@@ -1,10 +1,38 @@
 package AnalizadorSintactico.PrimerosSiguientes;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class PrimerosSiguientes {
 	
-	public static ResultadoPrimerosSiguientes hacer(Gramatica gramatica) {
+	/*public static ResultadoPrimerosSiguientes hacer(Gramatica gramatica) {
 
 		return new ResultadoPrimerosSiguientes();
+	}*/
+
+	static ArrayList<ConjuntoSimbolos> siguientes(ArrayList <String> simbolos, Gramatica gramatica){
+		ArrayList <ConjuntoSimbolos> resultados = new ArrayList<ConjuntoSimbolos>();
+		ArrayList <String> pruebas = new ArrayList<String>();
+		int tamañoVariable;
+
+		//Primer caso
+		resultados.get(0).setId(gramatica.simboloInicial);
+		resultados.get(0).getSimbolos().add("$");
+
+		for(ReglaProduccion buscar : gramatica.getReglaProduccion()){
+			for(String buscaProduccion : buscar.getProduccion()){
+				if(resultados.get(0).getId() == buscaProduccion){
+					resultados.get(0).get(buscaProduccion);
+					tamañoVariable = resultados.get(0).size();
+
+					if(resultados.get(0).get(buscaProduccion) == tamañoVariable-1){
+						/**/
+					}else if(simbolos.contains(resultados.get(0).get(buscaProduccion+1)) == true)){		
+						//primeros(resultados.get(0).get(buscaProduccion+1).contains(epsilon))
+					}
+				}
+			}
+		}
 	}
 
 }
