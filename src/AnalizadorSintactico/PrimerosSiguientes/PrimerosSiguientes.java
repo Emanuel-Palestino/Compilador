c3874f0f171f7c364346a10fe43a9d602c0232a5
MAARU20 <mario.aaru@gmail.com>
2021-11-18 21:19:12 -0600

    Beta Algoritmo Siguiente, Siguientes

--- src/AnalizadorSintactico/PrimerosSiguientes/PrimerosSiguientes.java
+++ src/AnalizadorSintactico/PrimerosSiguientes/PrimerosSiguientes.java
@@ -10,29 +10,48 @@ public class PrimerosSiguientes {
 		return new ResultadoPrimerosSiguientes();
 	}*/
 
-	static ArrayList<ConjuntoSimbolos> siguientes(ArrayList <String> simbolos, Gramatica gramatica){
-		ArrayList <ConjuntoSimbolos> resultados = new ArrayList<ConjuntoSimbolos>();
+
+	public static ConjuntoSimbolos siguiente (String simbolos){
+		
+	} 
+
+	static ArrayList <ConjuntoSimbolos> siguientes(String simbolos, Gramatica gramatica){
+		ArrayList <ConjuntoSimbolos> resultados = new ArrayList <ConjuntoSimbolos>();
 		ArrayList <String> pruebas = new ArrayList<String>();
-		int tamañoVariable;
+		int i,tamañoVariable, posicionA;
+		resultados.add(new ConjuntoSimbolos);
 
+		resultados.get(i).setId(gramatica.simboloInicial);
 		//Primer caso
-		resultados.get(0).setId(gramatica.simboloInicial);
-		resultados.get(0).getSimbolos().add("$");
-
-		for(ReglaProduccion buscar : gramatica.getReglaProduccion()){
-			for(String buscaProduccion : buscar.getProduccion()){
-				if(resultados.get(0).getId() == buscaProduccion){
-					resultados.get(0).get(buscaProduccion);
-					tamañoVariable = resultados.get(0).size();
-
-					if(resultados.get(0).get(buscaProduccion) == tamañoVariable-1){
-						/**/
-					}else if(simbolos.contains(resultados.get(0).get(buscaProduccion+1)) == true)){		
-						//primeros(resultados.get(0).get(buscaProduccion+1).contains(epsilon))
-					}
-				}
-			}
+		if (i == 0){
+			resultados.get(i).getSimbolos().add("$");
 		}
-	}
 
+		for(ReglaProduccion buscar : gramatica.getReglaProduccion()){			
+			posicionA = buscar.getReglaProduccion().IndexOf(buscar.getSimboloGramatical()); 
+			if ( posicionA >= 0){
+				tamañoVariable = buscar.getProduccion().size();
+				if (posicionA == tamañoVariable -1){
+					//Tercer caso
+
+
+				} else if (buscar.getProduccion().contains(gramatica.getNoTerminales()) == true){
+						if (buscar.getProduccion().get(posicionA+1).contains(gramatica.getNoTerminales()) == true){
+							//Pendiente  A-> A C
+							
+						} else {
+							if (resultados.get(i).getSimbolos().contains(buscar.getProduccion().get(posicionA+1)) == false){
+								//resultados.get(i).getSimbolos().add (buscar.getProduccion().get(posicionA+1));
+
+								//primeros(resultados.get(0).get(buscaProduccion+1).contains(epsilon))
+							} 
+							//Tercer caso 
+						}
+					}	else {				
+						}
+			}
+			i++;
+		}
+
+	}
 }
