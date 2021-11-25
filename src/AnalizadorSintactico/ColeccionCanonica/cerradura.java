68467b8f665353c9042925b3e4fe60c84fbd1d4a
snifex <noblseis88@gmail.com>
2021-11-24 21:49:38 -0600

    Obtenemos las reglas de simbolo2

--- src/AnalizadorSintactico/ColeccionCanonica/cerradura.java
+++ src/AnalizadorSintactico/ColeccionCanonica/cerradura.java
@@ -1,20 +1,29 @@
 package AnalizadorSintactico.ColeccionCanonica;
 
+import java.util.ArrayList;
+
 import Utilidades.ConjuntoElementos.ConjuntoElementos;
 import Utilidades.ConjuntoElementos.Elemento;
+import Utilidades.Gramatica.Gramatica;
+import Utilidades.Gramatica.ReglaProduccion;
 
 public class cerradura {
     public ConjuntoElementos hacer(ConjuntoElementos entradaElementos){
         String posicionElemento;
+        Gramatica gramatica = new Gramatica();
+        ArrayList <ReglaProduccion> reglasSimbolo = new ArrayList<ReglaProduccion>();
+
 
         ConjuntoElementos resultadosCerradura = new ConjuntoElementos();
         while(true){
             for(Elemento recorreElementos : entradaElementos.getElementos()){   //con este for recorremos los elementos que hay en entradaElementos
-                for(String recorrerProduccion : recorreElementos.getProduccion()){  //con este for recorremos las producciones de elementos
-                    posicionElemento = recorreElementos.getSimboloDespuesDePunto();
-                    
-
-                }
+                posicionElemento = recorreElementos.getSimboloDespuesDePunto();     //Obtenemos el elemento que esta despues de punto
+                reglasSimbolo = gramatica.reglasDeSimbolo(posicionElemento);
+                for(ReglaProduccion recorreReglasSimbolo : reglasSimbolo){
+                    for(String recorreProduccion : recorreReglasSimbolo.getProduccion()){
+                        
+                    }
+                }        //Con el objeto gramatica sacamos las reglas del elemento obtenido anteriormente
             }   
         }
     }
