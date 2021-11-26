5c9b508d33527f78873d2564cb2444e8166c31b1
CorvoHyatt <corvohyatt@gmail.com>
2021-11-25 20:21:06 -0600

    Coleccion Canonica

--- src/Utilidades/ColeccionCanonica.java
+++ src/Utilidades/ColeccionCanonica.java
@@ -1,8 +1,11 @@
 package Utilidades;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import Utilidades.ConjuntoElementos.ConjuntoElementos;
+import Utilidades.ConjuntoElementos.Elemento;
+import Utilidades.Gramatica.Gramatica;
 
 public class ColeccionCanonica {
 	private Set<ConjuntoElementos> conjuntosElementos;
@@ -21,5 +24,53 @@ public class ColeccionCanonica {
 	public void agregar(ConjuntoElementos conjunto) {
 		conjuntosElementos.add(conjunto);
 	}
+
+
+
+	//hacer clase gramatica ampliada
 	
+	public static ColeccionCanonica crear(Gramatica gramatica){
+
+
+		ColeccionCanonica coleccionCanonica = new ColeccionCanonica();
+
+		//crear regla 0
+		//---------------------------------------------------------------------------
+		Elemento elementoInicial = new Elemento();
+		ArrayList<String> produccion = new ArrayList<String>();
+		produccion.add(gramatica.getSimboloInicial());
+		produccion.add("$");
+		elementoInicial.setSimboloGramatical(gramatica.getSimboloInicial() +"'");
+		elementoInicial.setProduccion(produccion);
+		elementoInicial.insertarPuntoInicial();
+
+		//_--------------------------------------------------------------------------
+		coleccionCanonica.agregar(Cerradura.hacer(elementoInicial));
+		
+
+		for(ConjuntoElementos conjuntoElementosActual:  coleccionCanonica.getConjuntosElementos()){
+
+			//Cada simbolo gramatical
+			Set<String> simbolosGramaticales = new HashSet<String>();
+
+			for(Elemento elemento : conjuntoElementosActual.getElementos() ){
+
+				simbolosGramaticales.add(elemento.getSimboloDespuesDePunto());
+			}
+
+			ConjuntoElementos aux = new ConjuntoElementos(); 
+
+			for(String X : simbolosGramaticales){
+
+				aux.agregar(Ira.hacer(conjuntoElementosActual,X));
+
+				if(aux.getElementos().size() > 0){
+					
+					coleccionCanonica.agregar(aux);
+				}
+			}		
+		}
+		
+		return coleccionCanonica;
+	}
 }
