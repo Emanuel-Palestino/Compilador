5d107b703517723ad019e3b2c2dc7a06832aafcc
snifex <noblseis88@gmail.com>
2021-11-26 22:00:43 -0600

    Cerradura(Quite Inicial ')

--- src/AnalizadorSintactico/ColeccionCanonica/Cerradura.java
+++ src/AnalizadorSintactico/ColeccionCanonica/Cerradura.java
@@ -11,31 +11,16 @@ public class Cerradura {
         String B;
         ArrayList<ReglaProduccion> reglasSimbolo = new ArrayList<ReglaProduccion>();
         ConjuntoElementos resultadosCerradura = new ConjuntoElementos();
-        ArrayList<String> simboloInicialApostrofe = new ArrayList<String>();
-        String simboloApostrofeString;
-        simboloInicialApostrofe.add(gramatica.getSimboloInicial());     //Hacemos un arraylist que contendra el Simboloinicial' un ejemplo seria E'
-        simboloInicialApostrofe.add("'");                                       
-        simboloApostrofeString = String.join("", simboloInicialApostrofe);      //Convertimos el arraylist en un String para poder comparar
 
-        for(Elemento recorreElementos : entradaElementos.getElementos()) { // con este for recorremos los elementos que
-            if(recorreElementos.getSimboloGramatical().equals(simboloApostrofeString)){
-                for(String recorreNoTerminales : gramatica.getNoTerminales()){
-                    reglasSimbolo = gramatica.reglasDeSimbolo(recorreNoTerminales);
-                    for (ReglaProduccion recorreReglasSimbolo : reglasSimbolo) {
-                        Elemento auxiliarConvierte = new Elemento(recorreReglasSimbolo);
-                        auxiliarConvierte.insertarPuntoInicial();
-                        resultadosCerradura.agregar(auxiliarConvierte);
-                    }
-                }
-            }else{
-                B = recorreElementos.getSimboloDespuesDePunto();                //Obtenemos la posición de B                                                    
-                reglasSimbolo = gramatica.reglasDeSimbolo(B);                   //Obtenemos las reglas de produccion para B
-                for (ReglaProduccion recorreReglasSimbolo : reglasSimbolo) {
-                    Elemento auxiliarConvierte = new Elemento(recorreReglasSimbolo);
-                    auxiliarConvierte.insertarPuntoInicial();
-                    resultadosCerradura.agregar(auxiliarConvierte);
-                } 
-            }
+        for(Elemento recorreElementos : entradaElementos.getElementos()) { // con este for recorremos los elementos que      
+            B = recorreElementos.getSimboloDespuesDePunto();                //Obtenemos la posición de B                                                    
+            reglasSimbolo = gramatica.reglasDeSimbolo(B);                   //Obtenemos las reglas de produccion para B
+            for (ReglaProduccion recorreReglasSimbolo : reglasSimbolo) {
+                Elemento auxiliarConvierte = new Elemento(recorreReglasSimbolo);
+                auxiliarConvierte.insertarPuntoInicial();
+                resultadosCerradura.agregar(auxiliarConvierte);
+            } 
+            
             resultadosCerradura.agregar(recorreElementos);
             
         }
