573788287ae8efed801a4d587e05fa57e7f61d14
snifex <noblseis88@gmail.com>
2021-12-15 23:38:08 -0600

    Analisis Sintactico

--- src/AnalizadorSintactico/Analisis/AnalisisSintactico.java
+++ src/AnalizadorSintactico/Analisis/AnalisisSintactico.java
@@ -17,8 +17,10 @@ public class AnalisisSintactico {
 		Stack<String>[] pilaResultado = new Stack[100];
 		Stack<String> pila = new Stack<String>();
 		String casoD = new String();
+		int elementoTopePila; 
 		int a = 0; //Posicion actual de la tira de tokens
 		int iterador = 0; 
+		int index = 0;
 		boolean bandera = true;
 		pila.push("0");		//inicializamos la pila en 0
 
@@ -26,9 +28,9 @@ public class AnalisisSintactico {
 			String temp = tablaLR.getAcciones().get(Integer.parseInt(pila.peek())).get(tiraTokens.get(a));
 			String num = temp.substring(1);
 			char operacionSwitch = temp.charAt(0);
-			int index = Integer.parseInt(num);
 			switch(operacionSwitch){
 				case'd':
+					index = Integer.parseInt(num);
 					accionResultado [iterador] = "d" + index;
 					copiaPila.addAll(pila); //Copia pila
 					pilaResultado[iterador] = copiaPila ;
@@ -41,7 +43,8 @@ public class AnalisisSintactico {
 					iterador ++;
 					break;
 
-				case 'r':        /* reducir A -> β*/         
+				case 'r':        /* reducir A -> β*/  
+					index = Integer.parseInt(num);      
 					ReglaProduccion rProduccion = new ReglaProduccion();
 					rProduccion = gramatica.getReglasProduccion().get(index); // f -> id 
 					accionResultado [iterador] = "r" + index;
@@ -50,12 +53,12 @@ public class AnalisisSintactico {
 					pilaResultado[iterador] = copiaPila ;
 					entradaResultado[iterador].addAll(copiaTokens);
 					int tamañoPop = rProduccion.getProduccion().size()*2;
+					elementoTopePila = Integer.parseInt(pila.firstElement());	//elemento del tope de la pila antes del pop
 					for(int i = 0; i < tamañoPop ; i++){
 						pila.pop();
 					}
-					String j = pila.peek();
 					pila.push(rProduccion.getSimboloGramatical());
-					String s = tablaLR.getIrA().get(index).get(rProduccion.getSimboloGramatical());
+					String s = tablaLR.getIrA().get(elementoTopePila).get(rProduccion.getSimboloGramatical());
 					pila.push(s);
 					iterador++;
 					break;
@@ -73,7 +76,7 @@ public class AnalisisSintactico {
 					pilaResultado[iterador] = copiaPila;
 					accionResultado[iterador] = "Error sintáctico, se esperaba: ";
 					for(String simboloTerminal : gramatica.getTerminales()){
-						if(!tablaLR.getAcciones().get(index).get(simboloTerminal).equals("")){
+						if(!tablaLR.getAcciones().get(index).get(simboloTerminal).equals("$")){
 							accionResultado[iterador] += simboloTerminal + " ó ";
 						}
 					}
