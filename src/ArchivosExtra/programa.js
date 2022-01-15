5c45793be909a93f0ef18c13c30f70a401ff9316
MAARU20 <mario.aaru@gmail.com>
2022-01-14 19:14:19 -0600

    Gramatica Final,2 y 14 corregidas

--- src/ArchivosExtra/programa.js
+++ src/ArchivosExtra/programa.js
@@ -1,7 +1,68 @@
-var algo;
-function esto() {
-	while (true) {
-		ala
-	}
-	
+function a (lalo,toko){
+    while ( ! b ){
+        console.log ("Perras estupidas inician su nombre con D y su apellido con L")
+    }
+    if ( m < b || Y >= kassalask ){
+        function baladas (messi,ronaldo,debruyne){
+            "ATLAS ES CAMPEÓN"
+        }
+        console.log ("JALADAS")
+    }
+}
+console.log ("Hola")
+1+2
+switch ( hols ){
+    case m : l = k  
+    break
+    case n : l = k  
+    break
+    case o : l = k  
+    break
+    case p : l = k  
+    break
+    default : console.log ("opqwowkqop")
+}
+function a (lalo,toko){
+    while ( ! b ){
+        console.log ("Perras estupidas inician su nombre con D y su apellido con L")
+    }
+    if ( m < b || Y >= kassalask ){
+        function baladas (messi,ronaldo,debruyne){
+            "ATLAS ES CAMPEÓN"
+        }
+        console.log ("JALADAS")
+    }
+}
+function a (lalo,toko){
+    while ( ! b ){
+        console.log ("Perras estupidas inician su nombre con D y su apellido con L")
+    }
+    if ( m < b || Y >= kassalask ){
+        function baladas (messi,ronaldo,debruyne){
+            "ATLAS ES CAMPEÓN"
+        }
+        console.log ("JALADAS")
+    }
+}
+function a (lalo,toko){
+    while ( ! b ){
+        console.log ("Perras estupidas inician su nombre con D y su apellido con L")
+    }
+    if ( m < b || Y >= kassalask ){
+        function baladas (messi,ronaldo,debruyne){
+            "ATLAS ES CAMPEÓN"
+        }
+        console.log ("JALADAS")
+    }
+    function a (lalo,toko){
+        while ( ! b ){
+            console.log ("Perras estupidas inician su nombre con D y su apellido con L")
+        }
+        if ( m < b || Y >= kassalask ){
+            function baladas (messi,ronaldo,debruyne){
+                "ATLAS ES CAMPEÓN"
+            }
+            console.log ("JALADAS")
+        }
+    }
 }
