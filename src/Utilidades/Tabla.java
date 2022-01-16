810bfa368a36e1a9a4e489f3d025531c21cbf00f
Emanuel <emanuel.palestino.h@gmail.com>
2022-01-16 00:03:20 -0600

    [Tabla.java] pequeñas correciones

--- src/Utilidades/Tabla.java
+++ src/Utilidades/Tabla.java
@@ -14,12 +14,14 @@ import java.awt.Dimension;
 import java.awt.Font;
 
 public class Tabla extends JScrollPane {
-	DefaultTableModel modeloTabla;
-	DefaultTableCellRenderer celda;
-	JTable tabla;
-	Font fuente;
+	private DefaultTableModel modeloTabla;
+	private DefaultTableCellRenderer celda;
+	private JTable tabla;
+	private Font fuente;
+	private final int pixelesQuitar = 5;
 
-	public Tabla(int alto, int ancho) {
+
+	public Tabla(int ancho, int alto) {
 		// Iniciar fuente
 		fuente = new Font("Tahoma", Font.PLAIN, 12);
 
@@ -46,10 +48,10 @@ public class Tabla extends JScrollPane {
 			tabla.getColumnModel().getColumn(i).setCellRenderer(celda);
 
 		this.add(tabla);
-		this.setPreferredSize(new Dimension(alto, ancho));
+		this.setPreferredSize(new Dimension(ancho, alto));
 	}
 
-	public Tabla(int alto, int ancho, String[] encabezado, String[][] datos) {
+	public Tabla(int ancho, int alto, String[] encabezado, String[][] datos) {
 		// Iniciar fuente
 		fuente = new Font("Tahoma", Font.PLAIN, 12);
 
@@ -76,11 +78,11 @@ public class Tabla extends JScrollPane {
 			tabla.getColumnModel().getColumn(i).setCellRenderer(celda);
 
 		this.setViewportView(tabla);
-		this.setPreferredSize(new Dimension(alto, ancho));
+		this.setPreferredSize(new Dimension(ancho, alto));
 	}
 
 	// Tabla con titulo en la parte superior
-	public Tabla(int alto, int ancho, String[] encabezado, String[][] datos, String titulo) {
+	public Tabla(int ancho, int alto, String[] encabezado, String[][] datos, String titulo) {
 		// Iniciar fuente
 		fuente = new Font("Tahoma", Font.PLAIN, 12);
 
@@ -105,11 +107,11 @@ public class Tabla extends JScrollPane {
 		((DefaultTableCellRenderer) header.getDefaultRenderer()).setHorizontalAlignment(SwingConstants.CENTER);
 		for (int i = 0; i < columnas; i++) {
 			tabla.getColumnModel().getColumn(i).setCellRenderer(celda);
-			tabla.getColumnModel().getColumn(i).setMinWidth(ancho / encabezado.length - 3);
+			tabla.getColumnModel().getColumn(i).setMinWidth(ancho / encabezado.length - pixelesQuitar);
 		}
 
 		this.setViewportView(tabla);
-		this.setPreferredSize(new Dimension(alto, ancho));
+		this.setPreferredSize(new Dimension(ancho, alto));
 		this.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.gray), titulo,
 				TitledBorder.CENTER, TitledBorder.TOP));
 	}
@@ -121,7 +123,7 @@ public class Tabla extends JScrollPane {
 		int columnas = tabla.getColumnCount();
 		for (int i = 0; i < columnas; i++) {
 			tabla.getColumnModel().getColumn(i).setCellRenderer(celda);
-			tabla.getColumnModel().getColumn(i).setMinWidth(this.getWidth() / encabezado.length - 3);
+			tabla.getColumnModel().getColumn(i).setMinWidth(this.getWidth() / encabezado.length - pixelesQuitar);
 		}
 	}
 
