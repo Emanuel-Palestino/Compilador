e058183217eae2b751487d6f58a4ba10f206ae93
Rugal <scaryskriller@gmail.com>
2022-01-20 22:33:05 -0600

    añadiduras 20 01 2022

--- src/Utilidades/Gramatica/SimboloGramatical.java
+++ src/Utilidades/Gramatica/SimboloGramatical.java
@@ -1,22 +1,37 @@
 package Utilidades.Gramatica;
 
+import Utilidades.AnalizadorLexico.Token;
+
 public class SimboloGramatical {
     private String simboloGramatical;
     private String traduccion;
+    private String valorLexico;
+    private String temporal;
 
     //constructor
     public SimboloGramatical(){
         simboloGramatical = "";
         traduccion = "";
+        valorLexico="";
+        temporal="";
+        temporal="";
     }
 
     public SimboloGramatical(String simbolo){
         simboloGramatical = simbolo;
+        traduccion = "";
+        valorLexico="";
+        temporal="";
+        temporal="";
     }
 
+    //Constructores de copias
     public SimboloGramatical( SimboloGramatical simboloACopiar){
         this.simboloGramatical = simboloACopiar.simboloGramatical;
         this.traduccion = simboloACopiar.traduccion;
+    }
+    public SimboloGramatical( Token tokenACopiar){
+        this.valorLexico = tokenACopiar.getValorLexico();
     }
 
     //setters
@@ -26,6 +41,12 @@ public class SimboloGramatical {
     public void setTraduccion(String traduccion){
         this.traduccion = traduccion;
     }
+    public void setValorLexico(String valor){
+        this.valorLexico = valor;
+    }
+    public void setTemporal(String temporal){
+        this.temporal = temporal;
+    }
 
     //getters
     public String getSimboloGramatical(){
@@ -33,6 +54,12 @@ public class SimboloGramatical {
     }
     public String getTraduccion(){
         return traduccion;
+    }
+    public String getValorLexico(String valor){
+        return  valorLexico;
+    }
+    public String getTemporal(String temporal){
+        return temporal;
     }
 
 
