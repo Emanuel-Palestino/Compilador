c66803b2402e7d9c960e8d6120a3493a7f3193df
Emanuel <emanuel.palestino.h@gmail.com>
2022-01-27 18:34:57 -0600

    Acciones de gramática 1 y 2 listas. Pequeñas correciones hechas

--- src/AnalizadorSintactico/ConstruccionTablaLR/IniciarTablaLR.java
+++ src/AnalizadorSintactico/ConstruccionTablaLR/IniciarTablaLR.java
@@ -8,7 +8,7 @@ import Utilidades.Gramatica.Gramatica;
 public class IniciarTablaLR {
 
 	public IniciarTablaLR(JFrame parent) {
-		Gramatica grama = new Gramatica("src/ArchivosExtra/gramatica.txt");
+		Gramatica grama = new Gramatica("src/ArchivosExtra/GramaticaJS/gramaticajs1.txt");
 		ColeccionCanonica cc = ColeccionCanonica.hacer(grama);
 		TablaLR resultado = TablaLR.construir(cc, grama);
 		new VentanaTablaLR(parent, grama.stringSimbolosNoTerminales(), grama.stringSimbolosTerminales(), grama.getSimboloInicial(), grama.stringGramatica(), resultado, grama.getTerminales(), grama.getNoTerminales());
